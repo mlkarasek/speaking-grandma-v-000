24540509602407a63068eeb281ff00c734ba645b
Monica  Karasek <mlkarasek@gmail.com>
2017-10-18 04:54:58 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif phrase == phrase.upcase
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
 end
