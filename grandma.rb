b73a6e7b9032d19e9d4044bb27446e5ca8786202
Monica  Karasek <mlkarasek@gmail.com>
2017-10-18 04:43:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,9 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif string.upcase
+  elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
   end
 end
+
+phrase = I LOVE YOU GRANDMA
